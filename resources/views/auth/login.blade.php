26665b9a71cc8f083a6261936780981622f1e587
Rakibul2242 <mdrakibulislam2242@gmail.com>
2025-05-04 21:38:52 +0600

    login/signup edit password hide/show

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,16 +15,19 @@
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
+            <!-- Email Field -->
             <div>
                 <x-label for="email" value="{{ __('Email') }}" />
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             </div>
 
+            <!-- Password Field -->
             <div class="mt-4">
                 <x-label for="password" value="{{ __('Password') }}" />
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
+            <!-- Remember Me Checkbox -->
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-checkbox id="remember_me" name="remember" />
@@ -32,6 +35,7 @@
                 </label>
             </div>
 
+            <!-- Forgot Password Link -->
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
@@ -44,5 +48,13 @@
                 </x-button>
             </div>
         </form>
+
+        <!-- Sign Up Redirect Link -->
+        <div class="mt-4 text-center">
+            <span class="text-sm text-gray-600">
+                {{ __('Don\'t have an account?') }}
+                <a href="{{ route('register') }}" class="text-indigo-600 hover:text-indigo-900">{{ __('Sign Up') }}</a>
+            </span>
+        </div>
     </x-authentication-card>
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,26 +9,75 @@
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
+            <!-- Name Field -->
             <div>
                 <x-label for="name" value="{{ __('Name') }}" />
                 <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
 
+            <!-- Email Field -->
             <div class="mt-4">
                 <x-label for="email" value="{{ __('Email') }}" />
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             </div>
 
-            <div class="mt-4">
+            <!-- Password Field with Show/Hide -->
+            <div x-data="{ show: false }" class="mt-4 relative">
                 <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                <input :type="show ? 'text' : 'password'" id="password" name="password"
+                    class="block mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                    required autocomplete="new-password" />
+
+                <!-- Password Visibility Toggle Icon -->
+                <div class="absolute inset-y-0 right-3 top-9 flex items-center">
+                    <svg x-show="!show" @click="show = !show" xmlns="http://www.w3.org/2000/svg"
+                        class="h-5 w-5 text-gray-500 cursor-pointer" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-.233.762-.538 1.487-.902 2.166" />
+                    </svg>
+                    <svg x-show="show" @click="show = !show" xmlns="http://www.w3.org/2000/svg"
+                        class="h-5 w-5 text-gray-500 cursor-pointer" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.542-7a9.973 9.973 0 012.509-4.114m1.637-1.474A9.99 9.99 0 0112 5c4.478 0 8.268 2.943 9.542 7a9.959 9.959 0 01-1.62 2.918" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M3 3l18 18" />
+                    </svg>
+                </div>
             </div>
 
-            <div class="mt-4">
+            <!-- Confirm Password Field with Show/Hide -->
+            <div x-data="{ show: false }" class="mt-4 relative">
                 <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                <input :type="show ? 'text' : 'password'" id="password_confirmation" name="password_confirmation"
+                    class="block mt-1 w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                    required autocomplete="new-password" />
+
+                <!-- Password Confirmation Visibility Toggle Icon -->
+                <div class="absolute inset-y-0 right-3 top-9 flex items-center">
+                    <svg x-show="!show" @click="show = !show" xmlns="http://www.w3.org/2000/svg"
+                        class="h-5 w-5 text-gray-500 cursor-pointer" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-.233.762-.538 1.487-.902 2.166" />
+                    </svg>
+                    <svg x-show="show" @click="show = !show" xmlns="http://www.w3.org/2000/svg"
+                        class="h-5 w-5 text-gray-500 cursor-pointer" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.542-7a9.973 9.973 0 012.509-4.114m1.637-1.474A9.99 9.99 0 0112 5c4.478 0 8.268 2.943 9.542 7a9.959 9.959 0 01-1.62 2.918" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M3 3l18 18" />
+                    </svg>
+                </div>
             </div>
 
+            <!-- Terms and Conditions -->
             @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
                 <div class="mt-4">
                     <x-label for="terms">
@@ -46,6 +95,7 @@
                 </div>
             @endif
 
+            <!-- Register Button -->
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                     {{ __('Already registered?') }}
